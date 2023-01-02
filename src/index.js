cce976af66c1f03790019f2b21788599c5b9c607
Mauricio <mauhmg@gmail.com>
2023-01-02 11:54:48 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+
+  const user = users.find((user) => user.username == username )
+  
+  !user && response.status(400).json({ error: 'Conta não encontrada' })
+
+  request.user = user
+  return next()
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post('/users', checksExistsUserAccount, (request, response) => {
+  const { name, username } = request.body
+
+  users.push({id: uuidv4, name, username, todos: []})
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
